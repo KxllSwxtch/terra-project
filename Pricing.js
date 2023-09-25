4a31f65ec138d669a60fe49df092d72ba66915ea
KxllSwxtch <dimashin9@gmail.com>
2023-09-25 17:47:59 +0600

    updated pricing calculator logic

--- Pricing.js
+++ Pricing.js
@@ -9,6 +9,7 @@ const currency = document.getElementById('currency')
 const currencyTransfer = document.getElementById('currencyTransfer')
 const additionalFees = document.getElementById('additionalFees')
 const currencyAdditional = document.getElementById('currencyAdditional')
+const courseDurationSelect = document.getElementById('courseDuration')
 
 currencySelect.addEventListener('change', (event) => {
 	currency.innerHTML = event.target.value
@@ -17,6 +18,7 @@ currencySelect.addEventListener('change', (event) => {
 })
 
 // disable offline select if online selected
+let onlineSelectCurrentValue = 0
 onlineSelect.addEventListener('change', (event) => {
 	if (event.target.value !== 'default') {
 		offlineSelect.disabled = true
@@ -24,6 +26,11 @@ onlineSelect.addEventListener('change', (event) => {
 
 		enrollmentFees.innerHTML = 0
 		additionalFees.innerHTML = 0
+
+		// get a value from a string with course selection with the amount
+		// of hours per week
+		const value = event.target.value
+		onlineSelectCurrentValue = +value.match(/\d+/gm)[0]
 	} else {
 		offlineSelect.disabled = false
 		livingSelect.disabled = false
@@ -31,9 +38,13 @@ onlineSelect.addEventListener('change', (event) => {
 })
 
 // disable online select if offline selected
+let offlineSelectCurrentValue = ''
 offlineSelect.addEventListener('change', (event) => {
 	if (event.target.value !== 'default') {
 		onlineSelect.disabled = true
+
+		const value = event.target.value
+		offlineSelectCurrentValue = +value.match(/\d+/gm)[0]
 
 		currencySelect.addEventListener('change', (event) => {
 			currency.innerHTML = event.target.value
@@ -96,7 +107,18 @@ offlineSelect.addEventListener('change', (event) => {
 	}
 })
 
-// form change event
-pricingForm.addEventListener('change', (event) => {
-	console.log(event.target.name)
+let totalCourseHours = 0
+courseDurationSelect.addEventListener('change', (event) => {
+	const value = event.target.value
+
+	if (value === 'Short-term course lasting one month') {
+		totalCourseHours = onlineSelectCurrentValue * 4
+	}
+
+	if (value === 'Long-term course lasting two months') {
+		totalCourseHours = offlineSelectCurrentValue * 16
+	}
 })
+
+// form change event
+pricingForm.addEventListener('change', (event) => {})
